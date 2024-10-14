b584bfe15373b537d27fc60a6ce6cba28987ed31
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-10-15 01:33:58 +0300

    header-start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import GenAuthForm from "./components/GenAuthForm/GenAuthForm";
+import Header from "./components/Header/Header";
 
 function App() {
 
 
-  
+
   return (
     <>
+    <Header />
       <GenAuthForm />
     </>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import css from "./Header.module.css"
+
+const Header=()=>{
+	return (
+		<p className={css.p}>Header</p>
+	)
+}
+
+export default Header
