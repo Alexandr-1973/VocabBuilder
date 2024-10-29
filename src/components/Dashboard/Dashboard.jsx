0845b5a0311dfa2963f6065a0a72de6ec6eaf020
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-10-30 00:22:01 +0200

    add markup mob Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,33 @@
-import css from "./Dashboard.module.css"
-import Filters from "../Filters/Filters"
+import css from "./Dashboard.module.css";
+import Filters from "../Filters/Filters";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { GoPlus } from "react-icons/go";
 
-const Dashboard=()=>{
-	return (
-		<div className={css.genDiv}>
-			<Filters />
-		</div>
-		
-	)
-}
+const Dashboard = () => {
+  
+  return (
+    <div className={css.genDiv}>
+      <Filters />
+      <ul className={css.trainUl}>
+        <li className={css.studyLi}>
+          To study:<p className={css.studyP}>20</p>
+        </li>
+        <li className={css.buttonLi}>
+          <button className={css.wordButton}>
+            Add word
+            <GoPlus className={css.iconPlus} />
+          </button>
+        </li>
+        <li>
+          <Link to="/training" className={css.trainLink}>
+            Train oneself
+            <HiOutlineArrowNarrowRight className={css.iconArrow} />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
