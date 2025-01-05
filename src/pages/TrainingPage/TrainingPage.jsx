09b7fffa34004399408f5e1b5cb300609e7f2958
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2025-01-06 01:32:24 +0200

    prettier-Trp

--- src/pages/TrainingPage/TrainingPage.jsx
+++ src/pages/TrainingPage/TrainingPage.jsx
@@ -5,22 +5,20 @@ const TrainingPage = () => {
     <>
       <div className={css.emptyDiv}>
         <div className={css.textDiv}>
-          <h3 className={css.h3}>You don`t have a single word to learn right now.</h3>
+          <h3 className={css.h3}>
+            You don`t have a single word to learn right now.
+          </h3>
           <p className={css.p}>
-             Please create or add a word to start the workout. We want to improve
+            Please create or add a word to start the workout. We want to improve
             your vocabulary and develop your knowledge, so please share the
             words you are interested in adding to your study.
           </p>
-		  <div className={css.buttonDiv}>
-			<button className={css.addWordBtn}>Add word</button>
-			<button className={css.cancelBtn}>Cancel</button>
-
-
-		  </div>
+          <div className={css.buttonDiv}>
+            <button className={css.addWordBtn}>Add word</button>
+            <button className={css.cancelBtn}>Cancel</button>
+          </div>
         </div>
-        <div className={css.pictureDiv}>
-
-		</div>
+        <div className={css.pictureDiv}></div>
       </div>
     </>
   );
